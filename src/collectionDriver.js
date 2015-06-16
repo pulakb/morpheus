6d79adcfa3cc10dbb11426d326e543f24252341d
Pulak <bhattacharyyapulak@gmail.com>
2015-06-16 15:25:21 +0530

    First Commit

---
+++ src/collectionDriver.js
@@ -0,0 +1,210 @@
+var ObjectID = require('mongodb').ObjectID;
+var path = require("path");
+var url = require("url");
+var channelImage = [];
+CollectionDriver = function(db) {
+  this.db = db;
+};
+// Rovi
+
+CollectionDriver.prototype.getCollection = function(collectionName, callback) {
+  this.db.collection(collectionName, function(error, the_collection) {
+    if( error ) callback(error);
+    else callback(null, the_collection);
+  });
+};
+
+
+// Rovi
+
+CollectionDriver.prototype.find = function(collectionName, callback) {
+  this.getCollection(collectionName, function(error, the_collection) { //A
+      if( error ) callback(error)
+      else {
+	the_collection.find().toArray(function(error, result) {
+		if( error ) callback("err::"+error)
+		 else { console.log(result.length);
+			var res;				
+			if (result && result.length) {
+				res =result[0].created_at;	
+			}
+			callback(null, res); 
+		}
+		
+	});
+      }
+
+	});
+
+};
+
+//rovi
+
+//find all objects for a collection
+CollectionDriver.prototype.findAll = function(collectionName, callback) {
+    this.getCollection(collectionName, function(error, the_collection) { //A
+      if( error ) callback(error)
+      else {
+        the_collection.find({},{"channelsArray.channelId":1,"channelsArray.channelName":1,"channelsArray.channelNumber":1,"channelsArray.isFavourite":1,"channelsArray.imageReference":1,"channelsArray.created_at":1, "_id":0}).toArray(function(error, results) { //B
+          if( error ) callback(error)
+          else callback(null, results)
+        });
+      }
+    });
+};
+
+
+
+
+
+CollectionDriver.prototype.removeDuplicate = function(collectionName,channelList, callback) { 
+var channelImage;
+      this.getCollection(collectionName, function(error, the_collection) {
+        if (error) callback("error::::"+error)
+        else { var SourceId;
+
+		function herew(SourceId) {
+		the_collection.distinct("Programs", {"SourceId": SourceId},function(err, res) {
+			if(!err) {
+			var aa = res.sort(function(obj1, obj2) {
+				return new Date(obj1.AiringTime) - new Date(obj2.AiringTime);
+
+			});
+			the_collection.find({"SourceId":SourceId}, {"channelImage":1,"_id":0}).toArray( function(err, result) { 
+		if (!err) {channelImage = result[0].channelImage;
+
+	the_collection.remove({"SourceId":SourceId}, function(err, result) { 
+		if (!err) {
+		console.log("result:::");
+		the_collection.update({"SourceId":SourceId, "channelImage": channelImage},{$pushAll:{ "Programs": aa}},{upsert: true}, function() { 
+		});
+	}
+			
+		});
+
+
+
+
+ }
+});
+		
+
+			}
+			});
+}
+		for (var i =0; i < channelList.length; i++) { 
+			SourceId = channelList[i];
+			herew(SourceId);
+		}
+		
+	}
+      });
+};
+
+
+//rovi
+CollectionDriver.prototype.savePrograms = function(collectionName, obj, callback) {
+var SourceId;
+var Programs;
+    this.getCollection(collectionName, function(error, the_collection) { //A
+      if( error ) callback(error)
+      else {
+
+	for(var i = 0; i< obj.length; i++ ) {
+		SourceId = obj[i].SourceId;
+		Programs = obj[i].Airings;
+		if (obj[i] !== undefined && obj[i].ChannelImages.length == 1) { 
+		//console.log("yes........savePrograms."+obj.length);
+		var path = "/assets/channels_logo/"
+		var myUrl = obj[i].ChannelImages[0].ImageUrl;
+		var pathname = url.parse(myUrl).pathname;
+		var last = pathname.substring(pathname.lastIndexOf("/") + 1);	
+		channelImage = path + last;
+
+ 		}
+	  updateChannels(SourceId, Programs, channelImage);
+
+	}
+
+	callback();
+	function updateChannels(SourceId, Programs, channelImage) {
+	
+	the_collection.update({"SourceId":SourceId, "channelImage": channelImage},{$pushAll:{ "Programs": Programs}},{upsert: true}, function() {
+		});
+	}
+      }
+    });
+};
+
+
+//rovi
+//save new object
+CollectionDriver.prototype.save = function(collectionName, obj, callback) {
+ //var collName = "cloudDB1";
+    this.getCollection(collectionName, function(error, the_collection) { //A
+      if( error ) callback(error)
+      else {
+        //obj.created_at = new Date(); //B
+	//console.log(obj);
+        the_collection.insert({"name":"channelArray", "channels": obj, "created_at":new Date()}, function() { //C
+console.log("Insertedd...");          
+//the_collection.insert({"name":"page1"}, function(error) {
+	//if (error) {callback(error);}
+//});
+callback(null, obj);
+        });
+	
+      }
+    });
+};
+
+//rovi
+//delete a specific object
+CollectionDriver.prototype.delete = function(collectionName, callback) {
+    this.getCollection(collectionName, function(error, the_collection) { //A
+        if (error) callback(error)
+        else {
+            the_collection.remove(function(error,doc) { //B
+            	if (error) callback(error)
+            	else callback();
+            });
+        }
+    });
+}
+
+//rovi
+//Perform a collection query
+CollectionDriver.prototype.query = function(collectionName, query, callback) { //1
+console.log("calling the query function with query and collection " + JSON.stringify(query)  +":" + collectionName);
+    this.getCollection(collectionName, function(error, the_collection) { //2
+      if( error )
+	{
+      console.log(error);
+      callback(error);
+      }
+      else {
+        console.log("all is well so far");
+   
+        the_collection.find(query).toArray(function(error, results)
+
+ { //3
+          if( error ) 
+	{
+console.log("error in querry "+ error);		
+callback(error);
+}
+          else
+{
+console.log("all in well in query as well" + JSON.stringify(results));
+	 callback(null, results);
+}
+        });
+      }
+    });
+};
+
+
+
+
+
+exports.CollectionDriver = CollectionDriver;
---
+++ src/index.js
@@ -0,0 +1,223 @@
+var http = require('http'),
+    express = require('express'),
+    path = require('path'),
+    roviReq = require('request'),
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    CollectionDriver = require('./collectionDriver').CollectionDriver;
+ 
+var app = express();
+app.set('port', process.env.PORT || 3000); 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+//app.use(express.bodyParser()); // <-- add
+
+var mongoHost = 'localhost'; //A
+var mongoPort = 27017; 
+var collectionDriver;
+var collectionName = "rovi";
+var channelList = [];
+ 
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); //B
+
+mongoClient.open(function(err, mongoClient) { //C
+  if (!mongoClient) {
+      console.error("Error! Exiting... Must start MongoDB first");
+      process.exit(1); //D
+  }
+  var db = mongoClient.db("Cloud-WyPlay");  //E
+  collectionDriver = new CollectionDriver(db); //F
+  initialize();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+
+
+app.get('/', function (req, res) {
+res.send('<html><body><h1>Welcome to EPG Server</h1></body></html>');
+});
+
+
+app.get('/:collection/epgService/programLists', function(req, res, next) { 
+  var channelId = req.query.channelIds;
+  var userStartTime = req.query.userStartTime;
+  var userEndTime = req.query.userEndTime;
+
+
+  //var chanelName = req.query.namee;
+  var params = req.params;
+  console.log("Request Data"+params);
+  var entity = channelId;
+  console.log("channelName = "+channelId + " userStartTime = "+userStartTime+" userEndTime= " +userEndTime );
+  var collection = params.collection;
+  console.log("collection Data"+collection);
+  console.log("get all programlist:::::::::::::::"+channelId);
+   if (entity) {
+       collectionDriver.programList(collection, channelId,userStartTime,userEndTime,  function(error, objs) { //J
+          if (error) { res.send(400, error); }
+          else { res.send(200, objs); } //K
+       });
+   } else {
+      res.send(400, {error: 'bad url', url: req.url});
+   }
+
+
+});
+
+
+function initialize() {
+collectionDriver.find(collectionName,function(err, obj) {
+	var dt = new Date(obj).getUTCDate();
+	var da = new Date().getUTCDate();
+	if ((obj !== undefined) || (da !== dt)) {
+		collectionDriver.delete(collectionName,function() {
+			epgChannelList(function(channelList) {
+            console.log('------------------------------------------');
+            console.log(channelList);
+                console.log('------------------------------------------');
+			epgProgramList();
+
+			});
+		
+	     	});
+	}
+});
+
+};
+
+function epgChannelList(callback) {
+	var obj, collection = collectionName;
+	var url = "http://api.rovicorp.com/TVlistings/v9/listings/gridschedule/20394/info?locale=en-US&duration=30&includechannelimages=false&format=json&apikey=2pf79jxpfqsqjd2jcrcw65as";
+	rovicall(url, function(body){
+		var sourceId = "";
+		if (body){ data = JSON.parse(body);}
+		obj = data.GridScheduleResult.GridChannels;
+		//obj.created_at = new Date(); 
+		var count = 0;
+		for (var i = 0; i < obj.length; i++) {
+			channelList.push(obj[i].SourceId);
+			if (i == (obj.length -1)) {
+                callback(channelList);
+            }
+		}
+	});
+
+};
+ 
+function epgProgramList() {
+	var dt = new Date();
+	var utcTime = dt.toISOString();
+	dt.setUTCMinutes(00);dt.setUTCSeconds(00); dt.setUTCHours(00);dt.setUTCMilliseconds(00);
+	var urlList = [];
+
+	for (var i = 0; i < 6; i++) {
+		console.log("dt.toISOString::::"+dt.toISOString());//http://api.rovicorp.com/TVlistings/v9/listings/////
+		var url = "http://api.rovicorp.com/TVlistings/v9/listings/gridschedule/20394/info?apikey=s8brrx2spxjb82wy7w42s583&sig=sig&includechannelimages=true&locale=en-US&startdate="+dt.toISOString()+"&duration=30";
+		programListArray(url, 0, function(err1, obj1) {
+			
+		if (err1) { console.log(err1); }
+		   else {
+
+			collectionDriver.removeDuplicate(collectionName, channelList, function(err, obj) { 
+
+			if(!err) {
+                console.log("hhe");
+                console.log(obj);
+            }
+			});
+		   }
+
+
+
+		});
+		dt.setUTCHours(dt.getUTCHours() + 4);
+
+	}
+
+};
+
+//Display all program list
+function programListArray(url,i, callback) {
+		rovicall(url, function(body){
+		var data = JSON.parse(body);
+		var collection = collectionName;
+		obj = data.GridScheduleResult.GridChannels;
+            console.log(obj);
+		//obj.created_at = new Date(); 
+		collectionDriver.savePrograms(collection,obj, function(error, objs) {
+		   if (error) { console.log(error); }
+		   else {
+			if (i == 5){ console.log("noww");
+			callback();}
+		   }
+		});
+		
+	});	
+};
+
+
+function rovicall(url, callback) {
+console.log("rovicall");
+roviReq(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+	
+	callback(body);
+  }
+})
+
+};
+
+ 
+app.get('/:collection', function(req, res, next) {  
+   var params = req.params;
+   var query = req.query; //1
+console.log("before query " + req.params.collection);
+console.log("before query " + req.query.query);
+   
+   if (query) {
+        //query = JSON.parse(query); //2
+console.log("inside get with query"+query);
+        collectionDriver.query(req.params.collection, query, returnCollectionResults(req,res)); //3
+   } else {
+        collectionDriver.findAll(req.params.collection, returnCollectionResults(req,res)); //4
+   }
+});
+
+
+ 
+function returnCollectionResults(req, res) {
+    return function(error, objs) { //5
+	  	
+        if (error) {console.log("in error of returnCollectionResults"); res.send(400, error); }
+	        else { 
+                    if (!req.accepts('html')) { //6
+                        res.render('data',{objects: objs, collection: req.params.collection});
+                    } else {
+                        res.set('Content-Type','application/json');
+                        console.log("some data has been found");
+                        res.send(200, objs);
+                }
+        }
+    };
+}; 
+
+
+app.post('/:collection', function(req, res) { //A
+    var object = req.body;
+    var collection = req.params.collection;
+    collectionDriver.save(collection, object, function(err,docs) {
+          if (err) { res.send(400, err); } 
+          else { res.send(201, docs); } //B
+     });
+});
+
+
+ 
+app.use(function (req,res) {
+    res.render('404', {url:req.url});
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
